eb7ddf2f3f7cd645d4dcb520ad35e4dafadd516c
Zak Weston <zweston@MHC.local>
2016-10-06 16:52:18 -0400

    Regex needed for names

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,18 @@
 class Post < ActiveRecord::Base
 
-  validates :title,   presence: true
-  validates :content, presence: true
-  validates :name,    presence: true
+  # validates :title,   presence: true
+  # validates :content, presence: true
+  # validates :name,    presence: true
 
+  validate :flurb_regex
+
+  def flurb_regex
+    flurb_regex = (/\A.*fl\S{1,}rb.*\z/i)
+    if title =~ flurb_regex
+      errors.add(:title, (%("can't include the word "flurb" or a variation thereof"))
+    end
+    if content =~ flurb_regex
+      errors.add(:content, "can't include the word 'flurb' or a variation thereof")
+    end
+  end
 end
