376ddd2160101e3d2c595ded8a063ed6c13e67d3
Zachary Weston <zakweston@zacharys-mbp.attlocal.net>
2016-10-06 22:27:41 -0400

    Cannot pass rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,22 @@
 class Post < ActiveRecord::Base
 
-  # validates :title,   presence: true
-  # validates :content, presence: true
-  # validates :name,    presence: true
+  validates :title,   presence: true
+  validates :content, presence: true
+  validates :name,    presence: true
 
   validate :flurb_regex
 
   def flurb_regex
-    flurb_regex = (/\A.*fl\S{1,}rb.*\z/i)
+    flurb_regex = (/\Afl.?[ueo_].?rb\z/i)
+    name_regex = (/\A[^A-Za-z0-9,.\- ]\z/)
     if title =~ flurb_regex
-      errors.add(:title, (%("can't include the word "flurb" or a variation thereof"))
+      errors.add(:title, "can't include the word \"flurb\" or a variation thereof")
     end
     if content =~ flurb_regex
-      errors.add(:content, "can't include the word 'flurb' or a variation thereof")
+      errors.add(:content, "can't include the word \"flurb\" or a variation thereof")
+    end
+    if name =~ name_regex
+      errors.add(:name, "isn't a valid name (only letters, spaces, periods, dashes, and commas allowed)")
     end
   end
 end
